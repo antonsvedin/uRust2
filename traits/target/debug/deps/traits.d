9989cd73e5c47d9f9e7feec62e2f25e7b44c9f9a
Svedin <anton.svedin@sogeti.com>
2022-11-11 12:01:27 +0100

    traits

---
+++ traits/target/debug/deps/traits.d
@@ -0,0 +1,5 @@
+C:\Users\asvedin\dev\rust-tutorial\uRust2\traits\target\debug\deps\traits.exe: src\main.rs
+
+C:\Users\asvedin\dev\rust-tutorial\uRust2\traits\target\debug\deps\traits.d: src\main.rs
+
+src\main.rs:
---
+++ traits/target/debug/traits.d
@@ -0,0 +1,1 @@
+C:\Users\asvedin\dev\rust-tutorial\uRust2\traits\target\debug\traits.exe: C:\Users\asvedin\dev\rust-tutorial\uRust2\traits\src\main.rs
